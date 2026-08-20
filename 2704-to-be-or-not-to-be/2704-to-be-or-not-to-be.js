9ec061b3e2a2192e89b3e4e0e22284f2f7883ca9
Pedro Zappellini <62000959+PedroZappellini@users.noreply.github.com>
2026-08-20 13:47:27 -0300

    Time: 38 ms (85.88%), Space: 54 MB (24.84%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+
+    function toBe(newVal) {
+        if (val === newVal) {
+            return true
+        } else {
+            throw new Error("Not Equal")
+        }
+    }
+
+    function notToBe(newVal) {
+        if (val !== newVal) {
+            return true
+        } else {
+            throw new Error("Equal")
+        }
+    }
+
+    return {
+        toBe: toBe,
+        notToBe: notToBe
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
