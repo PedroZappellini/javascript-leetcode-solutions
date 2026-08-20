5442ebdf460caca8d6a53e72ada22d1ac0b13e86
Pedro Zappellini <62000959+PedroZappellini@users.noreply.github.com>
2026-08-20 13:48:52 -0300

    Time: 41 ms (70.82%), Space: 53.5 MB (57.01%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    const filteredArray = [];
+
+    for (i = 0; i < arr.length; i++) {
+        if (Boolean(fn(arr[i], i))) {
+            filteredArray.push(arr[i]);
+        }
+    }
+    
+    return filteredArray;
+};
